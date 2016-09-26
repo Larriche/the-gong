accd1a175fa5dfffc0673f8ecdaf05cd0ae263a6
Larriche <larriche99@gmail.com>
2016-09-25 20:58:16 -0700

    Wrote comments on the Comment model

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,43 +1,92 @@
 <?php
 class Comment
 {
-	protected $id;
+	/*
+    |--------------------------------------------------------------------------
+    | Comment Model
+    |--------------------------------------------------------------------------
+    |
+    | This model represents a comment on a news article 
+    |
+    */
 
+    // id of comment instance
+	protected $id;
+    
+    // name of comment poster
 	protected $poster;
 
+    // id of associated news article
 	protected $newsId;
 
+    // the content of the comment
 	protected $body;
 
+    // date comment was made
 	protected $datePosted;
 
+    // whether this comment instance was loaded from already existing
+    // info in database
 	public $loadedFromTable = false;
 
+    /**
+     * Set the id of this comment instance
+     * 
+     * @param $id the id of the comment as loaded from the database or 
+     * @return void
+     */
 	public function setId($id)
 	{
 		$this->id = $id;
 	}
 
+    /**
+     * Get the id of this comment instance
+     *  
+     * @return int $id id of the comment instance
+     */
 	public function getId()
 	{
 		return $this->id;
 	}
 
+    /**
+     * Set the name of the poster of this comment
+     * 
+     * @param $string name of poster 
+     * @return void
+     */
 	public function setPoster($posterName)
 	{
 		$this->poster = $posterName;
 	}
 
+    /**
+     * Get the name of the poster of this comment instance
+     *  
+     * @return string the name of the poster of this comment
+     */
 	public function getPoster()
 	{
 		return $this->poster;
 	}
-
+    
+    /**
+     * Set the id of the news associated with this comment
+     * 
+     * @param $id of the news article as loaded from the database  
+     * @return void
+     */
 	public function setNewsId($id)
 	{
 		$this->newsId = $id;
 	}
 
+    /**
+     * Get the id of the news article associated with this comment
+     *  
+     * @return int $id id of news article
+     */
 	public function getNewsId()
 	{
 		return $this->newsId;
